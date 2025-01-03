3df6400162daec67e68713e7c527edfb4d0b83fc
shashankhl <shashankhl280@gmail.com>
2025-01-03 07:34:44 +0530

    post final commit

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,25 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: "https://react-based-calendar-application.onrender.com",
+  headers: { "Content-Type": "application/json" },
 });
 
 // Companies API
-export const getCompanies = () => apiClient.get('/companies');
-export const addCompany = (data) => apiClient.post('/companies', data);
-export const updateCompany = (id, data) => apiClient.put(`/companies/${id}`, data);
+export const getCompanies = () => apiClient.get("/companies");
+export const addCompany = (data) => apiClient.post("/companies", data);
+export const updateCompany = (id, data) =>
+  apiClient.put(`/companies/${id}`, data);
 export const deleteCompany = (id) => apiClient.delete(`/companies/${id}`);
 
 // Communication Methods API
-export const getCommunicationMethods = () => apiClient.get('/communicationMethods');
+export const getCommunicationMethods = () =>
+  apiClient.get("/communicationMethods");
 export const addCommunicationMethod = async (newCommunication) => {
   try {
-    const response = await apiClient.post(`/communicationMethods`, newCommunication);
+    const response = await apiClient.post(
+      `/communicationMethods`,
+      newCommunication
+    );
     return response.data;
   } catch (error) {
     console.error("Error adding communication method:", error);
-    throw error;  // Rethrow the error to be handled by the caller
+    throw error; // Rethrow the error to be handled by the caller
   }
 };
 
@@ -29,16 +34,22 @@ export const updateCommunicationMethod = async (id, data) => {
     return response.data;
   } catch (error) {
     console.error("Error updating communication method:", error);
-    throw error;  // Rethrow the error to be handled by the caller
+    throw error; // Rethrow the error to be handled by the caller
   }
 };
 
 export const deleteCommunicationMethod = (id) =>
   apiClient.delete(`/communicationMethods/${id}`);
 
-export const addCommunicationToCompany = async (companyId, newCommunication) => {
+export const addCommunicationToCompany = async (
+  companyId,
+  newCommunication
+) => {
   try {
-    const response = await apiClient.post(`/companies/${companyId}/lastCommunications`, newCommunication);
+    const response = await apiClient.post(
+      `/companies/${companyId}/lastCommunications`,
+      newCommunication
+    );
     return response.data; // Return the updated company object
   } catch (error) {
     console.error("Error adding communication to company:", error);
